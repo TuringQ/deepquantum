c29a970650a0c178229d0a9cce6a4aeaf38a903f
Hugh <keminghu@outlook.com>
2024-07-09 14:48:14 +0800

    add hafnian function and torontonian supporting pytorch, add unit test comparing with the walrus

--- src/deepquantum/photonic/qmath.py
+++ src/deepquantum/photonic/qmath.py
@@ -8,7 +8,7 @@ from typing import Callable, Dict, List, Tuple
 import copy
 import numpy as np
 import torch
-from collections import defaultdict
+from collections import defaultdict, Counter
 from scipy import special
 from torch import vmap
 from tqdm import tqdm
@@ -341,3 +341,168 @@ def sample_sc_mcmc(prob_func: Callable,
         for key, value in dict_sample.items():
             merged_samples[key] += value
     return merged_samples
+
+def get_subsets(n):
+    '''Get powerset of [0, 1, ... , n-1]'''
+    subsets = [ ]
+    for k in range(n+1):
+        subset = [ ]
+        for i in itertools.combinations(range(n), k):
+            subset.append(list(i))
+        subsets.append(subset)
+    return subsets
+
+def integer_partition(m, n):
+    """ integer partition"""
+    results = [ ]
+    def back_trace(m, n, result=[]):
+        if m == 0:
+            results.append(result)
+            return
+        if m < 0 or n == 0:
+            return
+        back_trace(m, n - 1, result)
+        back_trace(m - n, n, result + [n])
+    back_trace(m, n)
+    return results
+
+def count_unique_permutations(nums):
+    def backtrack(counter):
+        if sum(counter.values()) == 0:
+            return 1
+        total_count = 0
+        for key in counter:
+            if counter[key] > 0:
+                counter[key] -= 1
+                total_count += backtrack(counter)
+                counter[key] += 1
+        return total_count
+    return backtrack(Counter(nums))
+
+def get_submat_haf(a, z):
+    '''Get submat for hafnian calculation'''
+    if not isinstance(z, torch.Tensor):
+        z = torch.tensor(z)
+    idx1 = 2 * z
+    idx2 = idx1+1
+    idx = torch.cat([idx1, idx2])
+    idx = torch.sort(idx)[0]
+    submat = a[idx][:, idx]
+    return submat
+
+def get_submat_tor(a, z):
+    '''Get submat for torontonian calculation'''
+    if not isinstance(z, torch.Tensor):
+        z = torch.tensor(z)
+    len_ = a.size()[-1]
+    idx1 = z
+    idx2 = idx1 + int((len_)/2)
+    idx = torch.cat([idx1, idx2])
+    idx = torch.sort(idx)[0]
+    if a.dim() == 1:
+        return a[idx]
+    if a.dim() == 2:
+        return a[idx][:, idx]
+
+def p_labda(submat, power, if_loop=False):
+    """
+    Return the coefficient of polynomial.
+    """
+    sigma_x = torch.tensor([[0, 1], [1, 0]])
+    len_ = int(submat.size()[-1]/2)
+    x_mat = torch.block_diag(*[sigma_x]*len_)
+    x_mat = x_mat.to(submat.dtype)
+    xaz = x_mat @ submat
+    eigen = torch.linalg.eig(xaz)[0] #eigen decomposition
+    trace_list = torch.stack([(eigen ** i).sum() for i in range(0, power+1)])
+    int_partition = integer_partition(power, power)
+    num_permu = []
+    for i in int_partition:
+        num_permu.append(count_unique_permutations(i))
+    if if_loop: #loop hafnian case
+        v = torch.diag(submat)
+        diag_contribution = torch.stack([v @ torch.linalg.matrix_power(xaz, i-1) @ x_mat @ v.reshape(-1,1)/2 for i in range(1, power+1)])
+        diag_contribution = diag_contribution.squeeze()
+        coeff =  0
+        for k in range(len(int_partition)):
+            prefactor = num_permu[k]
+            temp = int_partition[k]
+            poly_list = trace_list[temp]/(2 * torch.tensor(temp)) + diag_contribution[np.array(temp)-1]
+            poly_prod = poly_list.prod()
+            coeff = coeff + prefactor/special.factorial(len(temp)) * poly_prod
+    else:
+        coeff =  0
+        for k in range(len(int_partition)):
+            prefactor = num_permu[k]
+            temp = int_partition[k]
+            trace_prod = trace_list[temp].prod()
+            coeff = coeff + prefactor/special.factorial(len(temp)) * trace_prod / (2**len(temp)*torch.tensor(temp).prod())
+    return coeff
+
+def hafnian(A, if_loop = False, rtol=1e-05, atol=1e-06):
+    """
+    Calculate the hafnian for symmetrix matrix, using eigenvalue-trace method
+
+    See https://arxiv.org/abs/2108.01622 Eq.(B3)
+    """
+    if not isinstance(A, torch.Tensor):
+        A = torch.tensor(A)
+#     # vmap over torch.allclose isn't supported yet
+#     assert torch.allclose(A, A.mT, rtol=rtol, atol=atol), 'the input matrix should be symmetric'
+    size = A.size()[-1]
+    if size == 0:
+        return 1
+    if size == 2:
+        if if_loop:
+            return A[0, 1] + A[0, 0] * A[1, 1]
+        else:
+            return A[0, 1]
+    if A.size()[-1] % 2 == 1:  # consider odd case
+        A = torch.block_diag(torch.tensor(1), A)
+    power = len(A)//2
+    haf = 0
+    z_sets = get_subsets(power)
+    for i in range(1, len(z_sets)):
+        subset = z_sets[i]
+        num_ = len(subset[0])
+        sub_mats = torch.vmap(get_submat_haf, in_dims=(None, 0))(A, torch.tensor(subset))
+        coeff = torch.vmap(p_labda, in_dims=(0, None, None))(sub_mats, power, if_loop)
+        coeff_sum = (-1) ** (power-num_) * coeff.sum()
+        haf = haf + coeff_sum
+    return haf
+
+def _tor_helper(submat, sub_gamma):
+    size = submat.size()[-1]
+    temp = torch.eye(size)-submat
+    inv_temp = torch.linalg.inv(temp)
+    sub_gamma = sub_gamma.to(inv_temp.dtype)
+    exp_term  = sub_gamma @ inv_temp @ sub_gamma.conj()/2
+    return torch.exp(exp_term)/torch.sqrt(torch.linalg.det(temp + 0j))
+
+def torontonian(o_mat, gamma=None):
+    """
+    Calculate the torontonian function for given matrix.
+
+    See https://research-information.bris.ac.uk/ws/portalfiles/portal/329011096/thesis.pdf Eq.(3.54)
+    """
+    if not isinstance(o_mat, torch.Tensor):
+        o_mat = torch.tensor(o_mat)
+    if gamma is None:
+        gamma = torch.zeros(len(o_mat))
+    if not isinstance(gamma, torch.Tensor):
+        gamma = torch.tensor(gamma)
+    assert len(o_mat) % 2 == 0, 'input matrix dimension should be even '
+    m = len(o_mat) // 2
+    z_sets = get_subsets(m)
+    tor = (-1) ** m * torch.exp(gamma @ gamma.conj() / 2)
+    for i in range(1, len(z_sets)):
+        subset = z_sets[i]
+        num_ = len(subset[0])
+        sub_mats = torch.vmap(get_submat_tor, in_dims=(None, 0))(o_mat, torch.tensor(subset))
+        sub_gammas = torch.vmap(get_submat_tor, in_dims=(None, 0))(gamma, torch.tensor(subset))
+        coeff = torch.vmap(_tor_helper)(sub_mats, sub_gammas)
+        coeff_sum = (-1) ** (m - num_) * coeff.sum()
+        tor = tor + coeff_sum
+    return tor
+
+
--- tests/test_photonic_qmath.py
+++ tests/test_photonic_qmath.py
@@ -2,8 +2,9 @@ import deepquantum.photonic as dqp
 import networkx as nx
 import numpy as np
 import pytest
+import thewalrus
 import torch
-from deepquantum.photonic.qmath import quadrature_to_ladder, ladder_to_quadrature, takagi
+from deepquantum.photonic.qmath import quadrature_to_ladder, ladder_to_quadrature, takagi, hafnian, torontonian
 
 
 def test_quadrature_ladder_transform():
@@ -12,8 +13,8 @@ def test_quadrature_ladder_transform():
     matrix = torch.randn(2 * nmode, 2 * nmode)
     vector2 = ladder_to_quadrature(quadrature_to_ladder(vector))
     matrix2 = ladder_to_quadrature(quadrature_to_ladder(matrix))
-    assert torch.allclose(vector, vector2)
-    assert torch.allclose(matrix, matrix2)
+    assert torch.allclose(vector, vector2, atol=1e-5)
+    assert torch.allclose(matrix, matrix2, atol=1e-5)
 
 
 def test_takagi():
@@ -25,3 +26,41 @@ def test_takagi():
         s_diag = torch.diag(diag).to(u.dtype)
         assert torch.allclose(u @ u.mH, torch.eye(size) + 0j, rtol=1e-5, atol=1e-5)
         assert torch.allclose(u @ s_diag @ u.mT, a + 0j, rtol=1e-5, atol=1e-5)
+
+def test_hafnian():
+    n = 12
+    temp = np.random.rand(n, n)
+    A = temp + temp.transpose()
+    haf1 = thewalrus.hafnian(A, loop=False)
+    haf2 = hafnian(A, if_loop=False)
+    assert abs(haf1-haf2) < 1e-6
+
+def test_hafnian_loop():
+    n = 12
+    temp = np.random.rand(n, n)
+    A = temp + temp.transpose()
+    haf1 = thewalrus.hafnian(A, loop=True)
+    haf2 = hafnian(A, if_loop=True)
+    assert abs(haf1-haf2) < 1e-6
+
+
+def test_torontonian():
+    nmode = 15
+    cir = dqp.QumodeCircuit(nmode=nmode, init_state='vac', cutoff=5, backend='gaussian')
+    for i in range(nmode):
+        cir.s(wires=[i])
+        cir.d(wires=[i])
+    for i in range(nmode-1):
+        cir.bs(wires=[i,i+1])
+    cir.to(torch.double)
+
+    covs, means = cir()
+    cov_ladder = quadrature_to_ladder(covs[0])
+    mean_ladder = quadrature_to_ladder(means[0])
+    q = cov_ladder + torch.eye(2 * nmode) / 2
+    o_mat = torch.eye(2 * nmode) - torch.inverse(q)
+    tor1 = torontonian(o_mat)
+    tor2 =thewalrus.tor(o_mat.detach().numpy())
+    assert abs(tor1-tor2) < 1e-6
+
+
