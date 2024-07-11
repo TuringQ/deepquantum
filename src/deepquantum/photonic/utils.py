89d29b6bc5d7f8a5ac92bc947a06e70eda9e0f70
Hugh <keminghu@outlook.com>
2024-07-11 13:18:42 +0800

    update gbs examples

--- src/deepquantum/photonic/utils.py
+++ src/deepquantum/photonic/utils.py
@@ -2,6 +2,10 @@
 Utilities
 """
 
+import numpy as np
+import os
+import pickle
+import gzip
 import deepquantum.photonic as dqp
 
 
@@ -12,3 +16,26 @@ def set_hbar(hbar: float) -> None:
 def set_kappa(kappa: float) -> None:
     """Set the global kappa."""
     dqp.kappa = kappa
+
+def load_sample(filename):
+    """"load the sample data with the given filename"""
+    # dir_ = os.path.dirname(__file__)
+    # file_path = os.path.join(dir_, filename+'.pkl.gz')
+    with gzip.open(filename+'.pkl.gz','rb') as f:
+        sample = pickle.load(f)
+    return sample
+
+def save_sample(filename, data):
+    """"save the sample data with the given filename"""
+    with gzip.open(filename+'.pkl.gz','wb') as f:
+        pickle.dump(data, f)
+    return
+
+def load_adj(filename):
+    mat = np.load(filename+'.npy')
+    return mat
+
+def save_adj(filename, data):
+    """"save the adjacent matrix with the given filename"""
+    np.save(filename+'.npy', data)
+    return
