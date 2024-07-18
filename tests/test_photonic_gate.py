f1c7b7ddc3b0fe6b2943c92bef0ddc76f94fa860
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-07-18 00:07:27 -0500

    Optimize 'get_matrix_state' to improve numerical stability (#17) (#19)
    * Optimize 'get_matrix_state' to solve numerical instability (#17)
    
    * typo

--- tests/test_photonic_gate.py
+++ tests/test_photonic_gate.py
@@ -22,3 +22,35 @@ def test_2_mode_squeezing_gate():
     assert torch.allclose(cov1, cov2, atol=1e-6)
     assert torch.allclose(mean1, mean2, atol=1e-6)
     assert torch.allclose(sym1, sym2, atol=1e-6)
+
+
+def test_squeezing_gate_numerical_stability():
+    cutoff = 128
+    r = 1
+    cir1 = dq.QumodeCircuit(nmode=2, init_state='vac', cutoff=cutoff, backend='fock', basis=False)
+    cir1.s(0, r=r)
+    cir1.s(0, r=r)
+    cir1.to(torch.double)
+    state1 = cir1()
+
+    cir2 = dq.QumodeCircuit(nmode=2, init_state='vac', cutoff=cutoff, backend='fock', basis=False)
+    cir2.s(0, r=2 * r)
+    cir2.to(torch.double)
+    state2 = cir2()
+    assert torch.allclose(state1, state2)
+
+
+def test_2_mode_squeezing_gate_numerical_stability():
+    cutoff = 64
+    r = 1
+    cir1 = dq.QumodeCircuit(nmode=2, init_state='vac', cutoff=cutoff, backend='fock', basis=False)
+    cir1.s2([0,1], r=r)
+    cir1.s2([0,1], r=r)
+    cir1.to(torch.double)
+    state1 = cir1()
+
+    cir2 = dq.QumodeCircuit(nmode=2, init_state='vac', cutoff=cutoff, backend='fock', basis=False)
+    cir2.s2([0,1], r=2 * r)
+    cir2.to(torch.double)
+    state2 = cir2()
+    assert torch.allclose(state1, state2)
