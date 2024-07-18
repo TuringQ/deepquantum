b28c93052aa43e315c320d0e0f1d4258521ef765
Hugh <keminghu@outlook.com>
2024-07-18 10:14:00 +0800

    add loop torontonian test

--- tests/test_photonic_qmath.py
+++ tests/test_photonic_qmath.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 import thewalrus
 import torch
-from deepquantum.photonic.qmath import quadrature_to_ladder, ladder_to_quadrature, takagi, hafnian, torontonian
+from deepquantum.photonic.qmath import quadrature_to_ladder, ladder_to_quadrature, takagi, hafnian_torch, torontonian_torch
 
 
 def test_quadrature_ladder_transform():
@@ -32,7 +32,7 @@ def test_hafnian():
     temp = np.random.rand(n, n)
     A = temp + temp.transpose()
     haf1 = thewalrus.hafnian(A, loop=False)
-    haf2 = hafnian(A, if_loop=False)
+    haf2 = hafnian_torch(A, if_loop=False)
     assert abs(haf1-haf2) < 1e-6
 
 def test_hafnian_loop():
@@ -40,7 +40,7 @@ def test_hafnian_loop():
     temp = np.random.rand(n, n)
     A = temp + temp.transpose()
     haf1 = thewalrus.hafnian(A, loop=True)
-    haf2 = hafnian(A, if_loop=True)
+    haf2 = hafnian_torch(A, if_loop=True)
     assert abs(haf1-haf2) < 1e-6
 
 
@@ -59,8 +59,29 @@ def test_torontonian():
     mean_ladder = quadrature_to_ladder(means[0])
     q = cov_ladder + torch.eye(2 * nmode) / 2
     o_mat = torch.eye(2 * nmode) - torch.inverse(q)
-    tor1 = torontonian(o_mat)
+    tor1 = torontonian_torch(o_mat)
     tor2 =thewalrus.tor(o_mat.detach().numpy())
+    assert abs(tor1-tor2) < 1e-6
+
+def test_torontonian_loop():
+    nmode = 10
+    cir = dqp.QumodeCircuit(nmode=nmode, init_state='vac', cutoff=5, backend='gaussian')
+    for i in range(nmode):
+        cir.s(wires=[i])
+        cir.d(wires=[i])
+    for i in range(nmode-1):
+        cir.bs(wires=[i,i+1])
+    cir.to(torch.double)
+
+    covs, means = cir()
+    cov_ladder = quadrature_to_ladder(covs[0])
+    mean_ladder = quadrature_to_ladder(means[0])
+    q = cov_ladder + torch.eye(2 * nmode) / 2
+    gamma = mean_ladder.conj().mT @ torch.inverse(q)
+    gamma = gamma.squeeze()
+    o_mat = torch.eye(2 * nmode) - torch.inverse(q)
+    tor1 = torontonian_torch(o_mat, gamma)
+    tor2 =thewalrus.ltor(o_mat.detach().numpy(), gamma.detach().numpy())
     assert abs(tor1-tor2) < 1e-6
 
 
