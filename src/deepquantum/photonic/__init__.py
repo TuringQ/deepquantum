94a9b3e0c6600dcd54b4fd8e2b782e2928714460
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-08-01 03:33:51 -0500

    Update __init__.py

--- src/deepquantum/photonic/__init__.py
+++ src/deepquantum/photonic/__init__.py
@@ -7,11 +7,11 @@ from . import circuit
 from . import decompose
 from . import draw
 from . import gate
+from . import hafnian
 from . import mapper
 from . import operation
 from . import qmath
 from . import state
-from . import hafnian
 from . import torontonian
 
 from .ansatz import Clements, GaussianBosonSampling, GBS_Graph
@@ -20,9 +20,11 @@ from .decompose import UnitaryDecomposer
 from .draw import DrawClements
 from .gate import PhaseShift, BeamSplitter, MZI, BeamSplitterTheta, BeamSplitterPhi, BeamSplitterSingle, UAnyGate
 from .gate import Squeezing, Squeezing2, Displacement, DisplacementPosition, DisplacementMomentum
+from .hafnian import hafnian
 from .mapper import UnitaryMapper
 from .qmath import permanent, takagi, xxpp_to_xpxp, xpxp_to_xxpp, quadrature_to_ladder, ladder_to_quadrature
 from .state import FockState, GaussianState
+from .torontonian import torontonian
 from .utils import set_hbar, set_kappa
 
 hbar = 2
