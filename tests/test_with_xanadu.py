2600632664d26e037e4d68bf70b3c98dd35468bf
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-07-31 20:59:19 -0500

    Update test_with_xanadu.py

--- tests/test_with_xanadu.py
+++ tests/test_with_xanadu.py
@@ -6,30 +6,34 @@ from deepquantum.photonic.qmath import quadrature_to_ladder
 from deepquantum.photonic.hafnian import hafnian
 from deepquantum.photonic.torontonian import torontonian
 
+
 def test_hafnian():
     n = 12
     temp = np.random.rand(n, n)
-    A = temp + temp.transpose()
-    haf1 = thewalrus.hafnian(A, loop=False)
-    haf2 = hafnian(A, loop=False)
-    assert abs(haf1-haf2) < 1e-6
+    mat = temp + temp.transpose()
+    haf1 = thewalrus.hafnian(mat, loop=False)
+    mat = torch.tensor(mat)
+    haf2 = hafnian(mat, loop=False)
+    assert abs(haf1 - haf2) < 1e-6
+
 
 def test_hafnian_loop():
     n = 12
     temp = np.random.rand(n, n)
-    A = temp + temp.transpose()
-    haf1 = thewalrus.hafnian(A, loop=True)
-    haf2 = hafnian(A, loop=True)
-    assert abs(haf1-haf2) < 1e-6
+    mat = temp + temp.transpose()
+    haf1 = thewalrus.hafnian(mat, loop=True)
+    mat = torch.tensor(mat)
+    haf2 = hafnian(mat, loop=True)
+    assert abs(haf1 - haf2) < 1e-6
 
 
 def test_torontonian():
     nmode = 15
     cir = dqp.QumodeCircuit(nmode=nmode, init_state='vac', cutoff=5, backend='gaussian')
     for i in range(nmode):
-        cir.s(wires=[i])
-        cir.d(wires=[i])
-    for i in range(nmode-1):
+        cir.s(wires=i)
+        cir.d(wires=i)
+    for i in range(nmode - 1):
         cir.bs(wires=[i,i+1])
     cir.to(torch.double)
 
@@ -39,16 +43,17 @@ def test_torontonian():
     q = cov_ladder + torch.eye(2 * nmode) / 2
     o_mat = torch.eye(2 * nmode) - torch.inverse(q)
     tor1 = torontonian(o_mat)
-    tor2 =thewalrus.tor(o_mat.detach().numpy())
-    assert abs(tor1-tor2) < 1e-6
+    tor2 = thewalrus.tor(o_mat.detach().numpy())
+    assert abs(tor1 - tor2) < 1e-6
+
 
 def test_torontonian_loop():
     nmode = 10
     cir = dqp.QumodeCircuit(nmode=nmode, init_state='vac', cutoff=5, backend='gaussian')
     for i in range(nmode):
-        cir.s(wires=[i])
-        cir.d(wires=[i])
-    for i in range(nmode-1):
+        cir.s(wires=i)
+        cir.d(wires=i)
+    for i in range(nmode - 1):
         cir.bs(wires=[i,i+1])
     cir.to(torch.double)
 
@@ -60,5 +65,5 @@ def test_torontonian_loop():
     gamma = gamma.squeeze()
     o_mat = torch.eye(2 * nmode) - torch.inverse(q)
     tor1 = torontonian(o_mat, gamma)
-    tor2 =thewalrus.ltor(o_mat.detach().numpy(), gamma.detach().numpy())
-    assert abs(tor1-tor2) < 1e-6
+    tor2 = thewalrus.ltor(o_mat.detach().numpy(), gamma.detach().numpy())
+    assert abs(tor1 - tor2) < 1e-6
