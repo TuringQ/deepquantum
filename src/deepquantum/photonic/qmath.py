c36c6f62ee8169bffc1e345264279d7cd49df6d9
Jooyuza <827721892@qq.com>
2024-08-06 13:58:33 +0800

    typos

--- src/deepquantum/photonic/qmath.py
+++ src/deepquantum/photonic/qmath.py
@@ -65,7 +65,6 @@ def sub_matrix(u: torch.Tensor, input_state: torch.Tensor, output_state: torch.T
         warnings.filterwarnings('ignore') # local warning
         u1 = torch.repeat_interleave(u, output_state, dim=0)
         u2 = torch.repeat_interleave(u1, input_state, dim=1)
-    print('test')
     return u2
 
 def permanent(mat: torch.Tensor) -> torch.Tensor:
