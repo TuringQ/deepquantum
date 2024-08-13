fix numerical bugs for factorial

src/deepquantum/photonic/qmath.py
@@ -132,9 +132,7 @@ def permanent_ryser(mat: torch.Tensor) -> torch.Tensor:
 
 def product_factorial(state: torch.Tensor) -> torch.Tensor:
     """Get the product of the factorial from the Fock state, i.e., :math:`|s_1,s_2,...s_n> --> s_1!*s_2!*...s_n!`."""
-    dtype = state.dtype
-    return torch.exp(torch.lgamma(state.double() + 1).sum(-1, keepdim=True)).to(dtype) # nature log gamma function
-
+    return torch.exp(torch.lgamma(state.double() + 1).sum(-1, keepdim=True)).round() # nature log gamma function
 
 def fock_combinations(nmode: int, nphoton: int) -> List:
     """Generate all possible combinations of Fock states for a given number of modes and photons.

tests/test_init_state_batch.py
@@ -34,11 +34,11 @@ def test_batched_fock_basis_states():
 
         for key in res2.keys():
             # test is_prob=False
-            assert torch.allclose(res2[key], re2[key][i], atol=1e-5)
+            assert torch.allclose(res2[key], re2[key][i], atol=1e-6)
 
         for key in res3.keys():
             # test is prob = True
-            assert torch.allclose(res3[key], re3[key][i], atol=1e-5)
+            assert torch.allclose(res3[key], re3[key][i], atol=1e-6)
 
 
 def test_batched_fock_basis_states_and_data():
@@ -72,8 +72,8 @@ def test_batched_fock_basis_states_and_data():
 
         for key in res2.keys():
             # test is_prob=False
-            assert torch.allclose(res2[key], re2[key][i], atol=1e-5)
+            assert torch.allclose(res2[key], re2[key][i], atol=1e-6)
 
         for key in res3.keys():
             # test is prob = True
-            assert torch.allclose(res3[key], re3[key][i], atol=1e-5)
+            assert torch.allclose(res3[key], re3[key][i], atol=1e-6)