7025bffb1bbd996e0a8f7bdfa7f6791dbb8f4633
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-08-28 02:59:05 -0500

    Update measurement.py

--- src/deepquantum/photonic/measurement.py
+++ src/deepquantum/photonic/measurement.py
@@ -11,140 +11,149 @@ from torch import nn
 from torch.distributions.multivariate_normal import MultivariateNormal
 
 import deepquantum.photonic as dqp
-from .operation import Operation
 from .gate import PhaseShift
+from .operation import Operation
+
 
 class Generaldyne(Operation):
-    """Generaldyne measurement.
+    """General-dyne measurement.
 
     Args:
+        cov_m (Any): The covariance matrix for the general-dyne measurement.
         nmode (int, optional): The number of modes that the quantum operation acts on. Default: 1
         wires (List[int] or None, optional): The indices of the modes that the quantum operation acts on.
             Default: ``None``
-        covmat (Any): Performs the general-dyne measurement specified in covmat. Default: ``None``
         cutoff (int or None, optional): The Fock space truncation. Default: ``None``
-        name (str, optional): The name of the gate. Default: ``'Generaldyne'`
+        name (str, optional): The name of the measurement. Default: ``'Generaldyne'`
+        noise (bool, optional): Whether to introduce Gaussian noise. Default: ``False``
+        mu (float, optional): The mean of Gaussian noise. Default: 0
+        sigma (float, optional): The standard deviation of Gaussian noise. Default: 0.1
     """
     def __init__(
         self,
+        cov_m: Any,
         nmode: int = 1,
         wires: Union[int, List[int], None] = None,
-        covmat: Any = None,
         cutoff: Optional[int] = None,
-        name: str = 'Generaldyne'
+        name: str = 'Generaldyne',
+        noise: bool = False,
+        mu: float = 0,
+        sigma: float = 0.1
     ) -> None:
         self.nmode = nmode
         if wires is None:
             wires = list(range(nmode))
-        super().__init__(name=name, nmode=nmode, wires=wires, cutoff=cutoff, noise=False)
-        if not isinstance(covmat, torch.Tensor):
-            covmat = torch.tensor(covmat).reshape(-1, 2 * len(self.wires))
-        assert covmat.shape[-1] == 2 * len(self.wires), "Covariance matrix size does not match wires provided"
-        self.covmat = covmat
+        wires = self._convert_indices(wires)
+        if cutoff is None:
+            cutoff = 2
+        super().__init__(name=name, nmode=nmode, wires=wires, cutoff=cutoff, noise=noise, mu=mu, sigma=sigma)
+        if not isinstance(cov_m, torch.Tensor):
+            cov_m = torch.tensor(cov_m).reshape(-1, 2 * len(self.wires), 2 * len(self.wires))
+        assert cov_m.shape[-2] == cov_m.shape[-1] == 2 * len(self.wires), 'The size of cov_m does not match the wires'
+        self.register_buffer('cov_m', cov_m)
+        self.samples = None
 
     def forward(self, x: List[torch.Tensor]) -> List[torch.Tensor]:
         """Perform a forward pass for Gaussian states.
 
         See Quantum Continuous Variables: A Primer of Theoretical Methods
-        by Alessio Serafini page 121
+        by Alessio Serafini Eq.(5.143) and Eq.(5.144) in page 121
         """
-        cov, mean =  x
-        covmat = self.covmat
-        covmat = covmat.to(cov.dtype)
+        cov, mean = x
         wires = torch.tensor(self.wires)
         size = cov.size()
-        indices = torch.cat([wires, wires+self.nmode]) # xxpp order
-        all_idx = torch.arange(2*self.nmode)
-        mask = ~torch.isin(all_idx, indices)
-        idx_rest = all_idx[mask]
+        idx = torch.cat([wires, wires + self.nmode]) # xxpp order
+        idx_all = torch.arange(2 * self.nmode)
+        mask = ~torch.isin(idx_all, idx)
+        idx_rest = idx_all[mask]
 
-        cov_a = cov[:, idx_rest[:, None], idx_rest]
-        cov_b = cov[:, indices[:, None], indices]
-        cov_ab = cov[:, idx_rest[:, None], indices]
-        mean_b_ = mean[:, indices].squeeze(-1)
-        mean_a_ = mean[:, idx_rest].squeeze(-1)
-        temp = cov_ab @ torch.linalg.inv(cov_b + covmat)
-        cov_a2 = cov_a - temp @ cov_ab.mT # update the unmeasured part
-        cov_a3 = torch.stack([torch.eye(size[-1])]*size[0])
-        cov_a3 = cov_a3.to(cov_a2.dtype)
-        cov_a3[:, idx_rest[:, None], idx_rest] = cov_a2 # update the total cov mat
+        cov_a  = cov[:, idx_rest[:, None], idx_rest]
+        cov_b  = cov[:, idx[:, None], idx]
+        cov_ab = cov[:, idx_rest[:, None], idx]
+        mean_a = mean[:, idx_rest]
+        mean_b = mean[:, idx]
 
-        samples = MultivariateNormal(mean_b_, cov_b + covmat).sample([1]) # xxpp order, shape: (shots, batch, 2 * nwire) shots=1000?
-        samples = samples.permute(1, 0, 2)
-        temp2  = (samples[0] - mean_b_).unsqueeze(2)
-        mean_a2 = mean_a_.unsqueeze(2) + temp @ temp2
-        mean_new = torch.zeros_like(mean)
-        mean_new[:, idx_rest] = mean_a2
-        samples_half = samples[:,:,:len(self.wires)] # xxpp order
-        return samples_half, [cov_a3, mean_new]
+        cov_a = cov_a - cov_ab @ torch.linalg.solve(cov_b + self.cov_m, cov_ab.mT) # update the unmeasured part
+        cov_out = torch.stack([torch.eye(size[-1], dtype=cov.dtype, device=cov.dtype)] * size[0])
+        cov_out[:, idx_rest[:, None], idx_rest] = cov_a # update the total cov mat
+
+        mean_m = MultivariateNormal(mean_b.squeeze(-1), cov_b + self.cov_m).sample([1])[0] # (batch, 2 * nwire)
+        mean_a = mean_a + cov_ab @ torch.linalg.solve(cov_b + self.cov_m, mean_m - mean_b)
+        mean_out = torch.zeros_like(mean)
+        mean_out[:, idx_rest] = mean_a
+        self.samples = mean_m[:, :len(self.wires)] # xxpp order
+        return [cov_out, mean_out]
+
 
 class Homodyne(Generaldyne):
     """Homodyne measurement.
 
     Args:
-        inputs (Any, optional): The parameter of the gate. Default: ``None``
+        phi (Any, optional): The homodyne measurement angle. Default: ``None``
         nmode (int, optional): The number of modes that the quantum operation acts on. Default: 1
         wires (List[int] or None, optional): The indices of the modes that the quantum operation acts on.
             Default: ``None``
         cutoff (int or None, optional): The Fock space truncation. Default: ``None``
-        name (str, optional): The name of the gate. Default: ``'UAnyGate'`
+        eps (float, optional): The measurement accuracy. Default: 2e-4
+        requires_grad (bool, optional): Whether the parameter is ``nn.Parameter`` or ``buffer``.
+            Default: ``False`` (which means ``buffer``)
+        name (str, optional): The name of the gate. Default: ``'Homodyne'``
+        noise (bool, optional): Whether to introduce Gaussian noise. Default: ``False``
+        mu (float, optional): The mean of Gaussian noise. Default: 0
+        sigma (float, optional): The standard deviation of Gaussian noise. Default: 0.1
     """
     def __init__(
         self,
-        inputs: Any = None,
+        phi: Any = None,
         nmode: int = 1,
         wires: Union[int, List[int], None] = None,
         cutoff: Optional[int] = None,
+        eps: float = 2e-4,
+        requires_grad: bool = False,
         noise: bool = False,
         mu: float = 0,
         sigma: float = 0.1,
         name: str = 'Homodyne'
     ) -> None:
         self.nmode = nmode
-        self.noise = noise
-        self.mu = mu
-        self.sigma = sigma
-        wires = sorted(self._convert_indices(wires))
-        eps = 2e-4
-        covmat = torch.zeros(2 * len(wires))
-        covmat[:len(wires)] = eps ** 2
-        covmat[len(wires):] = 1/eps ** 2 # xxpp
-        covmat = torch.diag(covmat)
-        super().__init__(name=name, nmode=nmode, wires=wires, covmat=covmat, cutoff=cutoff)
+        if wires is None:
+            wires = [0]
+        wires = self._convert_indices(wires)
+        cov_m = torch.diag(torch.tensor([eps ** 2] * len(wires) + [1 / eps ** 2] * len(wires))) # xxpp
+        super().__init__(cov_m=cov_m, nmode=nmode, wires=wires, cutoff=cutoff, name=name,
+                         noise=noise, mu=mu, sigma=sigma)
         assert len(self.wires) == 1, f'{self.name} must act on one mode'
-        self.requires_grad = False
-        self.init_para(inputs)
+        self.requires_grad = requires_grad
+        self.init_para(inputs=phi)
         self.npara = 1
+
     def inputs_to_tensor(self, inputs: Any = None) -> torch.Tensor:
         """Convert inputs to torch.Tensor."""
         if inputs is None:
             inputs = torch.rand(len(self.wires)) * 2 * torch.pi
         elif not isinstance(inputs, (torch.Tensor, nn.Parameter)):
             inputs = torch.tensor(inputs, dtype=torch.float)
-        if inputs.dim() == 0:
-           inputs = inputs.unsqueeze(0)
+        inputs = inputs.reshape(-1)
         assert len(inputs) == len(self.wires)
         if self.noise:
             inputs = inputs + torch.normal(self.mu, self.sigma, size=(len(self.wires), )).squeeze()
         return inputs
+
     def init_para(self, inputs: Any = None) -> None:
         """Initialize the parameters."""
-        theta = self.inputs_to_tensor(inputs)
+        phi = self.inputs_to_tensor(inputs)
         if self.requires_grad:
-            self.theta = nn.Parameter(theta)
+            self.phi = nn.Parameter(phi)
         else:
-            self.register_buffer('theta', theta)
-
-    def extra_repr(self) -> str:
-        return f'wires={self.wires}, theta={self.theta.item()}'
+            self.register_buffer('phi', phi)
 
     def forward(self, x: List[torch.Tensor]) -> List[torch.Tensor]:
         """Perform a forward pass for Gaussian states."""
-        cov, mean =  x
-        theta = self.theta
-        r = PhaseShift(inputs=-theta, nmode=self.nmode, wires=self.wires, cutoff=self.cutoff)
-        sp_mat = r.get_symplectic()
-        sp_mat = sp_mat.to(cov.dtype)
-        cov = sp_mat @ cov @ sp_mat.mT
-        mean = sp_mat @ mean
+        cov, mean = x
+        r = PhaseShift(inputs=-self.phi, nmode=self.nmode, wires=self.wires, cutoff=self.cutoff)
+        r.to(cov.dtype).to(cov.device)
+        cov, mean = r([cov, mean])
         return super().forward([cov, mean])
+
+    def extra_repr(self) -> str:
+        return f'wires={self.wires}, theta={self.phi.item()}'
