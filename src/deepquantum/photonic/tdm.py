a2fafecd1567bfc51234829fa6dbeffa99836597
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-10-24 02:48:10 -0500

    Update tdm.py

--- src/deepquantum/photonic/tdm.py
+++ src/deepquantum/photonic/tdm.py
@@ -7,13 +7,6 @@ from typing import Any, List, Optional, Union
 import torch
 
 from .circuit import QumodeCircuit
-from .draw import DrawCircuit_TDM_global
-
-from .qmath import shift_func
-
-from collections import defaultdict
-from copy import copy, deepcopy
-
 
 
 class QumodeCircuitTDM(QumodeCircuit):
@@ -95,81 +88,3 @@ class QumodeCircuitTDM(QumodeCircuit):
             wires = self.wires
         wires = sorted(self._convert_indices(wires))
         return self.samples[..., wires, :]
-
-    def construct_global(self, shots):
-        """Construct the global circuit for TDM given shots."""
-        assert shots >=1, 'shots must be larger than 1'
-        self._prepare_unroll_dict()
-        self._unroll_circuit()
-        nmode1 = self.nmode
-        nmode2 = self._nmode_tdm
-        nmode3 = nmode2 + (shots-1) * nmode1
-        circ_global = QumodeCircuit(nmode=nmode3, init_state='vac', cutoff=self.cutoff, backend=self.backend, basis=self.basis)
-        circ_global.operators =  deepcopy(self._operators_tdm)
-        circ_global.measurements =  deepcopy(self._measurements_tdm)
-        sublists_ = deepcopy(list(self._unroll_dict.values()))
-        no_delay_list = [i[-1] for i in sublists_]
-        for i in range(len(circ_global.operators)): # shots=1 case
-            op  = circ_global.operators[i]
-            circ_global.npara += op.npara
-            op.nmode = nmode3
-            if self._operators_tdm[i] in self._encoders_tdm:
-                circ_global.encoders.append(op)
-                circ_global.ndata += op.npara
-        for i in range(len(circ_global.measurements)):
-            mea = circ_global.measurements[i]
-            circ_global.npara += mea.npara
-            mea.nmode = nmode3
-            if self._measurements_tdm[i] in self._encoders_tdm:
-                circ_global.encoders.append(mea)
-                circ_global.ndata += mea.npara
-
-        map_dict = defaultdict(list)
-        for i in range(nmode2, nmode3, nmode1):
-            for k in range(len(no_delay_list)):
-                map_dict[no_delay_list[k]].append(i+k)
-
-        for i in range(shots-1): # shots>1 case
-            for mea in self._measurements_tdm:
-                mea_copy = deepcopy(mea)
-                wires = mea_copy.wires
-                mea_copy.wires = [map_dict[wires[0]][i]] # single wire
-                mea_copy.nmode = nmode3
-                circ_global.measurements.append(mea_copy)
-                circ_global.npara += mea_copy.npara
-                if mea in self._encoders_tdm:
-                    circ_global.encoders.append(mea_copy)
-                    circ_global.ndata += mea_copy.npara
-        for i in range(shots-1):
-            for op in self._operators_tdm:
-                op_copy = deepcopy(op)
-                wires = op.wires
-                not_delay_wire = all([item in no_delay_list for item in wires]) # check if is the delay wire
-                if not_delay_wire:
-                    pos = [ ]
-                    for k in wires:
-                        pos.append(map_dict[k][i])
-                    op_copy.wires = pos
-                    op_copy.nmode = nmode3
-                    circ_global.operators.append(op_copy)
-                else:
-                    if len(wires)==2:
-                        idx = no_delay_list.index(wires[-1])
-                        temp = sublists_[idx]
-                        for k in range(len(temp)-1):
-                            if wires[0] in temp[k]:
-                                temp[k] = shift_func(temp[k], 1)
-                                new_wires = [temp[k][0], map_dict[wires[-1]][i]]
-                        op_copy.wires = new_wires
-                        op_copy.nmode = nmode3
-                        circ_global.operators.append(op_copy)
-                    if len(wires)==1:
-                        op_copy.wires = [temp[k][0]]
-                        op_copy.nmode = nmode3
-                        circ_global.operators.append(op_copy)
-                if op in self._encoders_tdm:
-                     circ_global.encoders.append(op_copy)
-                     circ_global.ndata += op_copy.npara
-                circ_global.npara += op_copy.npara
-        circ_global._draw_nstep = shots
-        return circ_global
