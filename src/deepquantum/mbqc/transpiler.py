4ba33f8e4201fb7561cd5484fa06614fceac9d22
Jooyuza <827721892@qq.com>
2024-10-31 16:27:04 +0800

    transpile x cnot

--- src/deepquantum/mbqc/transpiler.py
+++ src/deepquantum/mbqc/transpiler.py
@@ -1,4 +1,5 @@
 import deepquantum as dq
+from deepquantum.gate import SingleGate, DoubleGate, PauliX, CNOT
 
 def transpile(cir: dq.QubitCircuit):
     pattern = dq.Pattern(n_input_nodes=cir.init_state.nqubit, init_state=cir.init_state.state)
@@ -10,10 +11,10 @@ def gate_to_pattern(pattern, op):
     if isinstance(op, SingleGate):
         if isinstance(op, PauliX):
             pattern.pauli_x(input_node = pattern.nout_wire_dic[op.wires[0]])
-            pattern.nout_wire_dic[op.wires[0]] = pattern._bg_qubit
+            pattern.nout_wire_dic[op.wires[0]] = pattern._bg_qubit-1
 
     if isinstance(op, DoubleGate):
         if isinstance(op, CNOT):
-            pattern.cnot(control_node = pattern.nout_wire_dic[op.control],
-            target_node = pattern.nout_wire_dic[op.target])
-            pattern.nout_wire_dic[op.target] = pattern._bg_qubit
+            pattern.cnot(control_node = pattern.nout_wire_dic[op.wires[0]],
+            target_node = pattern.nout_wire_dic[op.wires[1]])
+            pattern.nout_wire_dic[op.wires[1]] = pattern._bg_qubit-1
