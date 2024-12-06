825b4d669606022bbad707dbab1cda20d896829c
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-12-06 11:00:22 +0800

    update MCMC, fix permanent and typos

--- src/deepquantum/photonic/utils.py
+++ src/deepquantum/photonic/utils.py
@@ -18,23 +18,23 @@ def set_kappa(kappa: float) -> None:
     dqp.kappa = kappa
 
 def load_sample(filename):
-    """"load the sample data with the given filename"""
+    """load the sample data with the given filename"""
     with gzip.open('./data/' + filename + '.pkl.gz','rb') as f:
         sample = pickle.load(f)
     return sample
 
 def save_sample(filename, data):
-    """"save the sample data with the given filename"""
+    """save the sample data with the given filename"""
     with gzip.open('./data/' + filename + '.pkl.gz','wb') as f:
         pickle.dump(data, f)
     return
 
 def load_adj(filename):
-    """"load the adjacent matrix with the given filename"""
+    """load the adjacent matrix with the given filename"""
     mat = np.load('./data/' + filename + '.npy')
     return mat
 
 def save_adj(filename, data):
-    """"save the adjacent matrix with the given filename"""
+    """save the adjacent matrix with the given filename"""
     np.save('./data/' + filename + '.npy', data)
     return
