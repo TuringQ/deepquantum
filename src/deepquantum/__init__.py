fbbc95575f0a7d3a1ff96a98f1ebfc7a8bca9507
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2024-12-18 13:25:09 +0800

    version 3.4.1

--- src/deepquantum/__init__.py
+++ src/deepquantum/__init__.py
@@ -3,7 +3,7 @@ This is the top level module from which all basic functions and classes of
 DeepQuantum can be directly imported.
 """
 
-__version__ = '3.4.0'
+__version__ = '3.4.1'
 
 
 from . import ansatz
