75867526c52108b08b9f629a43f9f5d7d48881b0
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2025-01-06 17:20:42 +0800

    Update test_partial_amplitudes.py

--- tests/test_partial_amplitudes.py
+++ tests/test_partial_amplitudes.py
@@ -2,28 +2,29 @@ import deepquantum as dq
 import pytest
 import torch
 
-def test_partial_amplitudes():
+
+def test_get_amplitude():
     n = 10
+    data = torch.randn(4, 3)
+    bits = '0101010101'
+
     cir1 = dq.QubitCircuit(nqubit=n, mps=False)
-    angles = 2*torch.pi*torch.rand(3)
     for i in range(n):
         cir1.h(i)
-        cir1.rx(i, angles[0])
-        cir1.ry(i, angles[1])
-        cir1.rz(i, angles[2])
-    for i in range(n-1):
-        cir1.cnot(i, i+1)
-    cir1()
-    amps_1 = cir1.get_partial_amplitudes(['0000000000', '1111111111', '0101010101'])
+        cir1.rx(i, encode=True)
+        cir1.ry(i, encode=True)
+        cir1.rz(i, encode=True)
+    cir2.cnot_ring()
+    cir1(data=data)
+    amp1 = cir1.get_amplitude(bits)
 
-    cir2 = dq.QubitCircuit(nqubit=n, mps=True, chi=5)
+    cir2 = dq.QubitCircuit(nqubit=n, mps=True, chi=8)
     for i in range(n):
         cir2.h(i)
-        cir2.rx(i, angles[0])
-        cir2.ry(i, angles[1])
-        cir2.rz(i, angles[2])
-    for i in range(n-1):
-        cir2.cnot(i, i+1)
-    cir2()
-    amps_2 = cir2.get_partial_amplitudes(['0000000000', '1111111111', '0101010101'])
-    assert abs(amps_1 - amps_2).max() < 1e-5
+        cir2.rx(i, encode=True)
+        cir2.ry(i, encode=True)
+        cir2.rz(i, encode=True)
+    cir2.cnot_ring()
+    cir2(data=data)
+    amp2 = cir2.get_amplitude(bits)
+    assert abs(amp1 - amp2).max() < 1e-5
