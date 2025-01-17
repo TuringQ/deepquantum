e36dea6e801f2f0963eb6f49ac0e324c7da3ec33
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2025-01-17 20:25:05 +0800

    support shots for expectation value

--- src/deepquantum/circuit.py
+++ src/deepquantum/circuit.py
@@ -17,7 +17,7 @@ from .gate import Rx, Ry, Rz, ProjectionJ, CNOT, Swap, Rxx, Ryy, Rzz, Rxy, Recon
 from .gate import UAnyGate, LatentGate, HamiltonianGate, Barrier
 from .layer import Observable, U3Layer, XLayer, YLayer, ZLayer, HLayer, RxLayer, RyLayer, RzLayer, CnotLayer, CnotRing
 from .operation import Operation, Gate, Layer, Channel
-from .qmath import amplitude_encoding, measure, expectation
+from .qmath import amplitude_encoding, measure, expectation, sample2expval
 from .state import QubitState, MatrixProductState
 
 
@@ -36,6 +36,7 @@ class QubitCircuit(Operation):
         mps (bool, optional): Whether to use matrix product state representation. Default: ``False``
         chi (int or None, optional): The bond dimension for matrix product state representation.
             Default: ``None``
+        shots (int, optional): The number of shots for measurement. Default: ``1024``
 
     Raises:
         AssertionError: If the type or dimension of ``init_state`` does not match ``nqubit`` or ``den_mat``.
@@ -48,12 +49,14 @@ class QubitCircuit(Operation):
         den_mat: bool = False,
         reupload: bool = False,
         mps: bool = False,
-        chi: Optional[int] = None
+        chi: Optional[int] = None,
+        shots: int = 1024
     ) -> None:
         super().__init__(name=name, nqubit=nqubit, wires=None, den_mat=den_mat)
         self.reupload = reupload
         self.mps = mps
         self.chi = chi
+        self.shots = shots
         self.set_init_state(init_state)
         self.operators = nn.Sequential()
         self.encoders = []
@@ -287,12 +290,16 @@ class QubitCircuit(Operation):
 
     def measure(
         self,
-        shots: int = 1024,
+        shots: Optional[int] = None,
         with_prob: bool = False,
         wires: Union[int, List[int], None] = None
     ) -> Union[Dict, List[Dict], None]:
         """Measure the final state."""
         assert not self.mps, 'Currently NOT supported.'
+        if shots is None:
+            shots = self.shots
+        else:
+            self.shots = shots
         if wires is None:
             wires = list(range(self.nqubit))
         self.wires_measure = self._convert_indices(wires)
@@ -302,7 +309,7 @@ class QubitCircuit(Operation):
             return measure(self.state, shots=shots, with_prob=with_prob, wires=self.wires_measure,
                            den_mat=self.den_mat)
 
-    def expectation(self) -> torch.Tensor:
+    def expectation(self, shots: Optional[int] = None) -> torch.Tensor:
         """Get the expectation value according to the final state and ``observables``."""
         assert len(self.observables) > 0, 'There is no observable'
         if isinstance(self.state, list):
@@ -312,9 +319,34 @@ class QubitCircuit(Operation):
             assert isinstance(self.state, torch.Tensor), 'There is no final state'
         assert self.wires_condition == [], 'Expectation with conditional measurement is NOT supported'
         out = []
-        for observable in self.observables:
-            expval = expectation(self.state, observable=observable, den_mat=self.den_mat, chi=self.chi)
-            out.append(expval)
+        if shots is None:
+            for observable in self.observables:
+                expval = expectation(self.state, observable=observable, den_mat=self.den_mat, chi=self.chi)
+                out.append(expval)
+        else:
+            self.shots = shots
+            dtype = self.state[0].real.dtype # in order to be compatible with MPS
+            device = self.state[0].device
+            for observable in self.observables:
+                cir_basis = QubitCircuit(nqubit=self.nqubit, den_mat=self.den_mat, mps=self.mps, chi=self.chi)
+                for wire, basis in zip(observable.wires, observable.basis):
+                    if basis == 'x':
+                        cir_basis.h(wire)
+                    elif basis == 'y':
+                        cir_basis.sdg(wire)
+                        cir_basis.h(wire)
+                cir_basis.to(dtype).to(device)
+                state = cir_basis(state=self.state)
+                samples = measure(state=state, shots=shots, wires=observable.wires, den_mat=self.den_mat)
+                if isinstance(samples, list):
+                    expval = []
+                    for sample in samples:
+                        expval_i = sample2expval(sample=sample).to(dtype).to(device)
+                        expval.append(expval_i)
+                    expval = torch.cat(expval)
+                elif isinstance(samples, dict):
+                    expval = sample2expval(sample=samples).to(dtype).to(device)
+                out.append(expval)
         out = torch.stack(out, dim=-1)
         return out
 
