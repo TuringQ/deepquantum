b9d0e6bae8aa4f23524023622cfb82982e75a8fe
Hugh <keminghu@outlook.com>
2026-01-13 14:32:59 +0800

    update function poly_lambda

--- src/deepquantum/photonic/hafnian_.py
+++ src/deepquantum/photonic/hafnian_.py
@@ -51,30 +51,31 @@ def get_submat_haf(a: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
     submat = a[idx][:, idx]
     return submat
 
-def trace_powers(a, power):
-    """
-    Compute [Tr(a^0), Tr(a^1), ..., Tr(a^power)] in one forward pass.
-    Autograd & Hessian safe.
-    """
-    traces = []
-    x = torch.diag(a.new_ones(a.shape[0]))
-    traces.append(torch.trace(x)) # a^0
-    for _ in range(power):
-        x = x @ a
-        traces.append(torch.trace(x))
-    return torch.stack(traces)
-
 def poly_lambda(submat: torch.Tensor, int_partition: List, power: int, loop: bool = False) -> torch.Tensor:
     """Get the coefficient of the polynomial."""
     size = submat.shape[-1]
     identity = torch.eye(size, dtype=submat.dtype, device=submat.device)
     x_mat = identity.reshape(size // 2, 2, size).flip(1).reshape(size, size)
     xaz = x_mat @ submat
-    trace_list = trace_powers(xaz, power)
+    # trace power calculation
+    traces = []
+    x = xaz.new_ones(xaz.shape[-1]).diag()
+    traces.append(torch.trace(x))
+    for _ in range(power):
+        x = x @ xaz
+        traces.append(torch.trace(x))
+    trace_list = torch.stack(traces)
     coeff = 0
     if loop: # loop hafnian case
         v = torch.diag(submat)
-        diag_term = torch.stack([v @ torch.linalg.matrix_power(xaz, i - 1) @ x_mat @ v / 2 for i in range(1, power+1)])
+        # matrix power calculation
+        diag_term = []
+        x = xaz.new_ones(xaz.shape[-1]).diag()
+        diag_term.append(v @ x @ x_mat @ v / 2)
+        for _ in range(power):
+            x = x @ xaz
+            diag_term.append(v @ x @ x_mat @ v / 2)
+        diag_term = torch.stack(diag_term)
     for orders in int_partition:
         ncount = count_unique_permutations(orders)
         orders = torch.tensor(orders, device=submat.device)
