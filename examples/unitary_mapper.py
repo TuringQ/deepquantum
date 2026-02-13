785c2c8aebef91bea3cba407ea32e544f73c7ec5
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2026-02-13 20:29:50 +0800

    jupytext sync

--- examples/unitary_mapper.py
+++ examples/unitary_mapper.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.19.0
+#       jupytext_version: 1.19.1
 #   kernelspec:
 #     display_name: dqp
 #     language: python
