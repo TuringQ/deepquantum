7271dbe154ec3ba0c35e9450f6f5d47a06f62f4c
sansiro77 <30891481+sansiro77@users.noreply.github.com>
2026-03-06 16:02:48 +0800

    version 4.5.0

--- src/deepquantum/__init__.py
+++ src/deepquantum/__init__.py
@@ -1,6 +1,6 @@
 """This is the top level module from which all basic functions and classes of DeepQuantum can be directly imported."""
 
-__version__ = '4.4.0'
+__version__ = '4.5.0'
 
 
 from . import (
